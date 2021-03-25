3fc22b9c224397e4480e883631417e4f9b736997
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2021-03-24 22:45:15 -0500

    Model Construction | Gabriel Blanco

---
+++ src/main/java/co/edu/unbosque/model/PersonDAO.java
@@ -0,0 +1,26 @@
+package co.edu.unbosque.model;
+
+import co.edu.unbosque.model.persistence.PersonDTO;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class PersonDAO {
+    private ArrayList<PersonDTO> persons = new ArrayList<>();
+
+    public void create(String fname, String mname, String lfname, String lmname, String hometown, String living, int id, Date birthday) {
+
+    }
+
+    public void read(int id) {
+
+    }
+
+    public void update(int id, String fname, String mname, String lfname, String lmname, String hometown, String living, Date birthday) {
+
+    }
+
+    public void delete(int id) {
+
+    }
+}
